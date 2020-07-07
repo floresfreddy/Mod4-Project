9186896259f35a5e67f48756ff7236ec036a8632
floresfreddy <floresfreddy.ultra@gmail.com>
2020-07-07 17:47:00 -0500

    working on stuff

--- dating-app/src/MainContainer.js
+++ dating-app/src/MainContainer.js
@@ -16,34 +16,30 @@ class MainContainer extends React.Component {
       terms: terms 
     })
 
-    fetch("http://localhost:3000/terms",
-      {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${localStorage.token}`,
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        } 
-      })
-      .then(res => res.json())
-      .then(data => 
-        {
-          console.log(data)
-      }
-      )
+    // fetch("http://localhost:3000/terms",
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       "Authorization": `Bearer ${localStorage.token}`,
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    //     } 
+    //   })
+    //   .then(res => res.json())
+    //   .then(data => 
+    //     {
+    //       console.log(data)
+    //   }
+    //   )
 
   }
-
-
-
-
 
   renderSwitch(param) {
       switch(param) {
         case 'match':
           return <Form setTerms={this.setTermsList}/>
         case 'matches': 
-          return <MatchTileList users={this.props.users}/>
+          return <MatchTileList users={this.props.users} terms={this.state.terms}/>
         case 'searchTerms': 
           return <SearchTermList terms={this.state.terms}/>
         default: 
