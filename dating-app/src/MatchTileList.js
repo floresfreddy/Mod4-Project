5a4dd123cf9a19d06f1a9a089466af4f0678a07d
floresfreddy <floresfreddy.ultra@gmail.com>
2020-07-08 16:09:31 -0500

    delete matches done

--- dating-app/src/MatchTileList.js
+++ dating-app/src/MatchTileList.js
@@ -5,10 +5,26 @@ import MatchTileCard from './MatchTileCard.js'
 
 class MatchTileList extends React.Component{
     
-    
+    state= {
+        filteredlist: null
+    }
+
+    deleteMatch=(deletedUser)=>{
+        if(!this.state.filteredlist){
+            this.setState({
+                filteredlist: this.props.users.filter(user=> user !== deletedUser)
+            })
+        }
+        else{
+            this.setState({
+                filteredlist: this.state.filteredlist.filter(user=> user !== deletedUser)
+            })
+        }
+    }
     
     
     render(){
+        if(!this.state.filteredlist){
         if (this.props.users.length === 0){
             return (
                 <h1>No Current Matches</h1>
@@ -19,11 +35,23 @@ class MatchTileList extends React.Component{
                 <br/>
                 <br/>
                 <Card.Group itemsPerRow={3}>
-                    {this.props.users.map(user=> <MatchTileCard user={user} allUsers={this.props.users}/>)}
+                    {this.props.users.map(user=> <MatchTileCard user={user} allUsers={this.props.users} deleteMatch={this.deleteMatch}/>)}
                 </Card.Group>
             </div>
            
         )
+        }
+        else{
+            return (
+                <div>
+                <br/>
+                <br/>
+                <Card.Group itemsPerRow={3}>
+                    {this.state.filteredlist.map(user=> <MatchTileCard user={user} allUsers={this.props.users} deleteMatch={this.deleteMatch}/>)}
+                </Card.Group>
+            </div>
+            )
+        }
           
     }
 
