9ea14a8b77c7fdc33f942b1d5cfa9b20d553c4f3
Stephanie Cheney <steph@scheneyphotography.com>
2020-07-07 22:56:50 -0500

    centered cards

--- dating-app/src/MatchTileCard.js
+++ dating-app/src/MatchTileCard.js
@@ -17,30 +17,9 @@ class MatchTileCard extends React.Component{
                             {this.props.user.bio}
                         </Card.Description>
                         </Card.Content>
-                        <Card.Content extra>
+                        <Card.Content extra className="center">
                             <Message/>
-                        {/* <Button onClick={this.message}>Message</Button> */}
-
-                        {/* <Modal dimmer="blurring" size="small" trigger={<Button>Message</Button>}>
-                            <Modal.Header></Modal.Header>
-                                <Modal.Content>
-                                    <Modal.Description>
-                                        <Header>LET'S CONNECT</Header>
-                                        <Segment inverted>
-    <Form inverted>
-      <Form.Group widths='equal'>
-        <Form.Input fluid label='First name' placeholder='First name' />
-        <Form.Input fluid label='Last name' placeholder='Last name' />
-      </Form.Group>
-      <Form.Checkbox label='I agree to the Terms and Conditions' />
-      <Button type='submit'>Submit</Button>
-    </Form>
-  </Segment>
-                                    </Modal.Description>
-                                </Modal.Content>
-                        </Modal> */}
-
-                        <Button>Delete</Button>
+                            <Button>Delete</Button>
                         </Card.Content>
                     </Card>
             </div>
