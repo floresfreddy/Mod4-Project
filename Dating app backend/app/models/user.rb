15accdc5e78054d743c57b6ad3c1c76ef8f9aa30
Stephanie Cheney <steph@scheneyphotography.com>
2020-07-08 13:10:00 -0500

    created Messages component

--- Dating app backend/app/models/user.rb
+++ Dating app backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     has_many :terms 
+    has_many :messages, :class_name=> 'Message', :foreign_key =>'sender_id'
+    has_many :messages, :class_name=> 'Message', :foreign_key =>'user_id'
 end
--- Dating app backend/config/routes.rb
+++ Dating app backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   resources :terms, only: [:create, :index, :update]
   # localhost:3000/api/v1/login
   namespace :api do
